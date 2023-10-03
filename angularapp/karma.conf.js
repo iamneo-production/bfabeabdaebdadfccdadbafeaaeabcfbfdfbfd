45710e82369766d98d31b39fd8bfd84a184e446b
Iamneo <techadmin@iamneo.ai>
2023-10-03 13:15:34 +0000

    Backup Commit

--- angularapp/karma.conf.js
+++ angularapp/karma.conf.js
@@ -1,6 +1,4 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
+const puppeteer = require('puppeteer');
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -9,24 +7,43 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false 
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angularapp'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    jasmineHtmlReporter: {
+      suppressAll: true 
     },
-    reporters: ['progress', 'kjhtml'],
+    specReporter: {
+      maxLogLines: 5,   
+      suppressErrorSummary: true,
+      suppressFailed: false,    
+      suppressPassed: false,   
+      suppressSkipped: true,  
+      showSpecTiming: false,  
+      failFast: true,    
+      prefixes: {
+        success: '    OK: ',   
+        failure: 'FAILED: ',    
+        skipped: 'SKIPPED: '   
+      }
+    },
+    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    browsers: ['CustomChromeHeadless'],
+    customLaunchers: {
+      CustomChromeHeadless: {
+        base: 'Chrome',
+        flags: ['--headless','--disable-gpu','--remote-debugging-port=9222','--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath: '/usr/bin/chromium-browser'
+      },
+    },
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
